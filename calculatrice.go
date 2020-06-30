5a1b70f743399ec1b66e8be0da7098038e9adfbe
clemenceauriacombe <clemence.auriacombe@gmail.com>
2020-06-30 15:50:12 +0200

    calculatrice opérationnelle

---
+++ calculatrice.go
@@ -0,0 +1,32 @@
+package main 
+
+import "fmt"
+
+func main(){
+	var operator string  
+	var number1, number2 float64 
+	fmt.Print("Please enter First number: ")  
+	fmt.Scanln(&number1)  
+	fmt.Print("Please enter Second number: ")  
+	fmt.Scanln(&number2)  
+	fmt.Print("Please enter Operator (+,-,/,%,*):")  
+	fmt.Scanln(&operator)  
+	output := 0.0  
+	switch operator {  
+	case "+":  
+	 output = number1 + number2  
+	 break  
+	case "-":  
+	 output = number1 - number2  
+	case "*":  
+	 output = number1 * number2  
+	case "/":  
+	 output = number1 / number2  
+	 
+	default:  
+	 fmt.Println("Invalid Operation")  
+	}  
+	fmt.Printf("%f %s %f = %f\n", number1, operator, number2, output)  
+   }  
+
+
